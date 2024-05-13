8cb26b19b78b442c5dfa49b822a93cadff083d50
Mantequilla53 <77872710+Mantequilla53@users.noreply.github.com>
2024-05-13 15:30:56 -0400

    trade up tab changes

--- crafted.js
+++ crafted.js
@@ -4,22 +4,23 @@ function showCraftedContent(description, entries, contentContainer, tabStatsCont
     'Mil-Spec': 0,
     'Restricted': 0,
     'Classified': 0,
-    'Covert': 0 
-  }
+    'Covert': 0
+  };
   let totalTradeUp = 0;
 
   entries.forEach((entry) => {
     const { plusItems } = entry;
     plusItems.forEach((item) => {
       const tradeUpType = item.itemType;
-      if (tradeUpTypeCount.hasOwnProperty(tradeUpType)){
+      if (tradeUpTypeCount.hasOwnProperty(tradeUpType)) {
         tradeUpTypeCount[tradeUpType]++;
       }
     });
   });
+
   tabStatsContainer.innerHTML = `
     <link rel="stylesheet" href="style/craft.css">
-    <h3>Trade-Up</h3>
+    <h3>${description}</h3>
     <div>
       ${Object.entries(tradeUpTypeCount)
         .map(([type, count]) => {
@@ -46,50 +47,75 @@ function showCraftedContent(description, entries, contentContainer, tabStatsCont
 
     contentContainer.innerHTML = '';
     entries.forEach((entry) => {
-      const { d, t, plusItems, minusItems } = entry;
-      const filteredPlusItems = plusItems.filter((item) => selectedTradeUpTypes.includes(item.itemType));
+      const filteredPlusItems = entry.plusItems.filter((item) => selectedTradeUpTypes.includes(item.itemType));
       if (filteredPlusItems.length > 0) {
+        const { d, t, plusItems, minusItems } = entry;
+        const takenColor = extractItemColor(minusItems[0].itemType);
         const entryElement = document.createElement('div');
         entryElement.classList.add('entry');
         entryElement.innerHTML = `
-  <div class="entry-header">
-    <span>${d} ${t}</span>
-  </div>
-  <div class="card-container">
-    ${plusItems.length > 0 ? `
-      <div class="card given-item">
-        <div class="card-color" style="background-color: ${extractItemColor(plusItems[0].itemType)}"></div>
-        <div class="given-item-image">
-          <img src="images/${plusItems[0].itemName}.png">
-        </div>
-        <div class="given-item-text">
-          <p>${plusItems[0].market_name}</p>
-        </div>
-      </div>
-    ` : ''}
-    <div class="taken-items-container">
-      ${minusItems.map(item => `
-        <div class="card taken-item">
-          <div class="card-color" style="background-color: ${extractItemColor(item.itemType)}"></div>
-          <img src="images/${item.itemName}.png">
-          <p>${item.market_name}</p>
-          ${item.stickers && item.stickers.length > 0 ? `
-            <div class="stickers-section">
-              ${item.stickers.map(sticker => `
-                <img class="sticker-image" src="images/${sticker.imgSrc}.png">
-              `).join('')}
+            <div class="entry-header">
+              <span>${d} ${t}</span>
             </div>
-          ` : ''}
-        </div>
-      `).join('')}
-    </div>
-  </div>
-`;
+            <div class="card-container">
+              ${plusItems.length > 0 ? `
+                <div class="card given-item">
+                  <div class="card-color" style="background-color: ${extractItemColor(plusItems[0].itemType)}"></div>
+                  <div class="given-item-image">
+                    <img src="images/${plusItems[0].itemName}.png">
+                  </div>
+                  <div class="given-item-text">
+                    <p>${plusItems[0].market_name}</p>
+                  </div>
+                </div>
+              ` : ''}
+              <div class="taken-items-container">
+                ${minusItems.map(item => `
+                  <div class="card taken-item">
+                    <div class="card-color" style="background-color: ${takenColor}"></div>
+                    <div class="card-image-container">
+                      <img src="images/${item.itemName}.png">
+                      ${item.stickers && item.stickers.length > 0 ? `
+                        <div class="item-separator"></div>
+                        <div class="stickers-section">
+                          ${item.stickers.map(sticker => `
+                            <img class="sticker-image" src="images/${sticker.imgSrc}.png">
+                          `).join('')}
+                        </div>
+                      ` : ''}
+                    </div>  
+                    <p>${item.market_name}</p>
+                  </div>
+                `).join('')}
+              </div>
+            </div>
+            <div class="new-content"></div>
+          <button class="generate-button">
+            <span>Generate Possible Outputs</span>
+            <i class="fas fa-chevron-down"></i>
+          </button>
+        `;
         contentContainer.appendChild(entryElement);
+
+        const generateButton = entryElement.querySelector('.generate-button');
+        const dropContent = entryElement.querySelector('.new-content');
+        generateButton.addEventListener('click', () => {
+          dropContent.classList.toggle('show');
+          if (dropContent.classList.contains('show')) {
+            const outputText = possibleOutputs(minusItems);
+            dropContent.textContent = outputText;
+          } else {
+            dropContent.textContent = '';
+          }
+        });
       }
     });
   }
   updateContentContainer();
+}
+
+function possibleOutputs(minusItems) {
+  return 'Coming soon';
 }
 
 function extractItemColor(itemType) {
@@ -105,5 +131,5 @@ function extractItemColor(itemType) {
 }
 
 module.exports = {
-    showCraftedContent
+  showCraftedContent
 };
